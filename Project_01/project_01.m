9990c4e640e77a88c5f28e320a8c22d98210843c
LAPTOP-65NL20MD\geoff <Clark.M.Geoffrey@gmail.com>
2017-09-17 20:54:35 -0700

    Actually working project01

--- Project_01/project_01.m
+++ Project_01/project_01.m
@@ -1,5 +1,6 @@
 %% Project 1 Clark Lakshminarayanan Sonawani
 clear all
+close all
 
 %% Generate Clusters
 
@@ -9,6 +10,16 @@ clear all
 [Train1Dn4,Train2Dn4,Test1Dn4,Test2Dn4]=GenerateClusters(-4);
 % Training and Testing clusters for d = -8
 [Train1Dn8,Train2Dn8,Test1Dn8,Test2Dn8]=GenerateClusters(-8);
+
+% % Organize Data in Clusters
+TrainData={Train1D2,Train2D2;
+           Train1Dn4,Train2Dn4;
+           Train1Dn8,Train2Dn8};
+
+TestData={Test1D2,Test2D2;
+          Test1Dn4,Test2Dn4;
+          Test1Dn8,Test2Dn8};
+
 
 
 %% Plot Clusters
@@ -40,6 +51,64 @@ scatter(Test2Dn8(1,:),Test2Dn8(2,:),20,'b','filled')
 legend('Cluster 1 Training','Cluster 2 Training','Cluster 1 Testing','Cluster 2 Testing')
 
 
+
+%% Train all
+
+% trainAlgorithms={'traingd','traingdm','trainlm'};
+Test.Algorithms={'traingd','traingdm','trainlm'};
+Test.d=[2,-4,-8];
+Test.Lrate=0.5;
+Test.Nneurons=15;
+
+for n=1:length(Test.Algorithms)
+    for m=1:length(Test.d)
+        rng(10);
+        % prep training input
+        y1 = ones(1,1000);
+        y2 = zeros(1,1000);
+        train_set = [TrainData{m,1},TrainData{m,2}];
+        order = randperm(2000);
+        train_set = train_set(:,order);
+        target = [y1,y2];
+        target = target(order);
+
+        % setup net
+        net{m,n} = feedforwardnet(Test.Nneurons,Test.Algorithms{n});
+        net{m,n} = configure(net{m,n},train_set,target);
+        net{m,n}.trainParam.lr = Test.Lrate;
+        net{m,n}.trainParam.epochs=16000;% Number of Iterations
+        net{m,n}.divideParam.trainRatio = 0.75;
+        net{m,n}.divideParam.valRatio = 0.25;
+        net{m,n}.trainParam.max_fail=2000;
+        if m==2
+            net{m,n}.trainParam.mc = 0.1;% momentum rate
+        end
+        
+        % training algorithm
+        net{m,n} = train(net{m,n},train_set,target);
+        classifier{m,n} = GenerateBoundary(net{m,n});
+        train_op = net{m,n}(train_set);
+
+        % testing algorithm
+        order = randperm(1000);
+        testing_set = [TestData{m,1},TestData{m,2}];
+        testing_set = testing_set(:,order);
+        test_op = net{m,n}(testing_set);
+        
+    end
+end
+
+
+%% Diplay
+for m=1:length(Test.d)
+
+    figure(m)
+    hold on;grid on;
+    plot(classifier{m,1}(1,:),classifier{m,1}(2,:))
+    plot(classifier{m,2}(1,:),classifier{m,2}(2,:))
+    plot(classifier{m,3}(1,:),classifier{m,3}(2,:))
+
+end
 
 
 
