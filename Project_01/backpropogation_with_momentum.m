cf8a0bafa2f6b0497ee0c11abf58b2a47fdcfa49
Deathstroke22 <shubham.sonawani@asu.edu>
2017-09-17 18:04:11 -0700

    Update backpropogation_with_momentum.m

--- Project_01/backpropogation_with_momentum.m
+++ Project_01/backpropogation_with_momentum.m
@@ -1,5 +1,5 @@
 clear all
-
+clc
 %% Generate Clusters
 
 % Training and Testing clusters for d = 2
@@ -44,23 +44,25 @@ X1=[Train1D2 Train2D2];
 X2=[Train1Dn4 Train2Dn4];
 X3=[Train2Dn8 Train2Dn8];
 y = [zeros(1000,1);ones(1000,1)]';
-A=[X1; y]
-B=[X2; y]
-C=[X3; y]
+A=[X1; y];
+B=[X2; y];
+C=[X3; y];
 rand=randperm(2000,2000);
 A=A(:,rand); %randmized inputs and targets
 B=B(:,rand);
 C=C(:,rand);
 
-net = feedforwardnet([35],'trainrp');%%resilient backpropogation with momentum
+net = feedforwardnet(5,'trainrp');%%resilient backpropogation with momentum
 % net_1 = feedforwardnet([10],'trainrp');
 % net_1 = feedforwardnet([10],'trainrp');
 
 net = configure(net,A(1:2,:),A(3,:) );
 net.trainParam.epochs=4000;% Number of Iterations
-net.trainParam.lr = 0.001;% learning rate
-net.trainParam.mc = 0.9;% momentum rate
-net.trainParam.max_fail=2000;
+net.trainParam.lr = 0.3;% learning rate
+net.divideParam.trainRatio = 0.75;
+net.divideParam.valRatio = 0.25;
+net.trainParam.mc = 0.7;% momentum rate
+net.trainParam.max_fail=20000;
 net = init(net);
 view(net);
 
@@ -70,7 +72,7 @@ net_2=train(net,B(1:2,:),B(3,:));
 net_3=train(net,C(1:2,:),C(3,:));
 
 
-X_test1=[Test1D2 Test2D2]
+X_test1=[Test1D2 Test2D2];
 output_1= net_1(X_test1);
 for i=1:1000
     if(output_1(i)>=0.5)
@@ -81,7 +83,7 @@ for i=1:1000
 end
 
 
-X_test2=[Test1Dn4 Test2Dn4]
+X_test2=[Test1Dn4 Test2Dn4];
 output_2= net_2(X_test2);
 for i=1:1000
     if(output_2(i)>=0.5)
@@ -91,7 +93,7 @@ for i=1:1000
     end
 end
 
-X_test3=[Test1Dn8 Test2Dn8]
+X_test3=[Test1Dn8 Test2Dn8];
 output_3= net_3(X_test3);
 for i=1:1000
     if(output_3(i)>=0.5)
@@ -112,4 +114,5 @@ figure(6);
 gscatter(X_test3(1,:),X_test3(2,:),output_3,'rb','o+');
  
 legend('class 1','class 2')
+
 
