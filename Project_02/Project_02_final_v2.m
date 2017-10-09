ec53599f0bc459d7bc675bdf84f4f229019bd519
Venkatavaradhan V Lakshminarayanan <vvlakshm@asu.edu>
2017-10-08 22:33:27 -0700

    final changes made

--- Project_02/Project_02_final_v2.m
+++ Project_02/Project_02_final_v2.m
@@ -1,6 +1,9 @@
+%% Project - 2
 clear all
-% close all
-%% EDITABLE VALUES
+close all
+clc
+
+%% Editable Values
 windowSize=10; % Number of data points in the input window
 predictionSize=1; % Number of data points in the  output window
 testSize=30; % Number of data points withheld for testing
@@ -11,8 +14,6 @@ Train.Hneurons=[10 5]; % Number of neurons used in net
 Train.Niterations=1000; % Number of total epochs to run (set low for early stopping)
 
 %% Setup
-% FILENAME='3-project_time series data_students.xlsx';
-% vector=xlsread(FILENAME,strcat('A1:A275'));
 load('data.mat')
 totalSize=windowSize+predictionSize;
 vecLen=length(vector);
@@ -21,7 +22,7 @@ totalShift=totalSize-1;
 Best.err=50000;
 Best.net=fitnet(Train.Hneurons,Train.Algorithm);
 Best.seed=0;
-% figure(1);grid on;plot(vector);
+
 %% Organize Data
 trainStart=windowSize+1;
 trainEnd=vecLen;
@@ -34,15 +35,6 @@ end
 
 testData=vector(testStart:testEnd)';
 
-% fig10=figure(10);hold on;grid on;
-% set(fig10,'units','points','position',[200,550,1200,300])
-% % plot(vector);
-% for i=0:shiftSize
-%     plot((trainStart+i:trainEnd-shiftSize+i),trainData(windowSize+1+i,:)','lineWidth',predictionSize+1-i);
-%     plot((testStart:testEnd),testData)
-%     
-% end
-
 %% Train all
 u=25;
 rng(u);
@@ -52,11 +44,9 @@ m=1;
 order = randperm(length(trainData));
 randtrainData = trainData(:,order);
 
-
 % setup net
 net{m} = fitnet(Train.Hneurons,Train.Algorithm);
 net{m} = configure(net{m},randtrainData(1:windowSize,:),randtrainData(windowSize+1:end,:));
-%     net{m}.trainParam.lr = Train.Lrate;
 net{m}.trainParam.mu = 0.005;
 net{m}.trainParam.epochs=Train.Niterations;% Number of Iterations
 net{m}.divideParam.trainRatio = 0.95;
@@ -64,13 +54,11 @@ net{m}.divideParam.valRatio = 0.05;
 net{m}.divideParam.testRatio = 0.0;
 net{m}.trainParam.max_fail=50;
 
-
-% training algorithm
+% Training algorithm
 net{m} = train(net{m},randtrainData(1:windowSize,:),randtrainData(windowSize+1:end,:));
 
-%% testing
+%% Testing
 train_result = net{m}(trainData(1:windowSize,:));
-
 test_vec=trainData(trainEnd-windowSize+1:trainEnd);
 
 for i=1:predictionSize:testSize
@@ -78,14 +66,10 @@ for i=1:predictionSize:testSize
     test_vec(windowSize+i:windowSize+i-1+predictionSize) = test_result(1,(i:i-1+predictionSize));
 end
 
-
-
 %% MSE Training Calculation
-% errTrain = immse(trainData(windowSize+1,:),train_result(1,:));
 errTest = immse(testData,test_result);
 
 %% Save Best
-
 if errTest<Best.err
     Best.err=errTest;
     Best.net=net{m};
@@ -102,9 +86,7 @@ for i=1:predictionSize:testSize+29
     test_vec(windowSize+i:windowSize+i-1+predictionSize) = test_result(1,(i:i-1+predictionSize));
 end
 
-
 %% Plot Best
-
 fig1=figure(1);
 fig1.Renderer='Painters';
 set(fig1,'units','points','position',[200,450,1200,300]);
@@ -113,20 +95,17 @@ title('Neural Net Output vs Given Data');
 xlabel('Time [Year]');
 ylabel('Magnitude');
 xlim([0 310]);
-% plot actual
 plot(vector,'k','lineWidth',2)
 for i=0:shiftSize
     plot((trainStart+i:trainEnd-shiftSize+i),train_result(i+1,:)','r','lineWidth',1.2);
 end
-% plot(vector,'Color',[.5 .5 .5],'lineWidth',2)
 plot((testStart:testEnd+29),test_result,'b','lineWidth',2)
-% plot((testEnd:testEnd+30),test_result(end-30:end),'b','lineWidth',2)
 legend('Time Series Data','Neural Net Output on Training Data','Neural Net Prediction','Location','northwest')
 print('-painters','-depsc','figure2')
 print('-painters','-dpdf','figure2')
+
 %% Final Test Vector
 Best.final_vector=test_result(end-30+1:end)
-perform(Best.net)
 
 
 
