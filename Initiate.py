fa7d93ba8587d8158fd251644a4d47c8ede85669
Venkatavaradhan V Lakshminarayanan <vvlakshm@asu.edu>
2017-10-26 13:38:13 -0700

    Update Initiate.py

--- Initiate.py
+++ Initiate.py
@@ -9,16 +9,18 @@ import reinforcement_learning as rl
 tf.__version__
 gym.__version__
 
+# choose the game that you want to play
+# Also if you want to play breakout-v0, use another trained model from here: 
+# https://drive.google.com/a/asu.edu/file/d/0B1N26drdz1d7RVdNckpDQW83aWs/view?usp=sharing
+
 env_name = 'Breakout-v0'
 #env_name = 'SpaceInvaders-v0'
 rl.checkpoint_base_dir = 'checkpoints_tutorial16/'
 
-
 rl.update_paths(env_name=env_name)
 
-
+# uncomment this code, if you want the check points to be downloaded
 #rl.maybe_download_checkpoint(env_name=env_name)
-
 
 agent = rl.Agent(env_name=env_name,
                  training=True,
@@ -28,22 +30,7 @@ agent = rl.Agent(env_name=env_name,
 model = agent.model
 replay_memory = agent.replay_memory
 agent.run(num_episodes=1)
-#log_q_values = rl.LogQValues()
-#log_reward = rl.LogReward()
-#
-#log_q_values.read()
-#log_reward.read()
-#
-#plt.plot(log_reward.count_states, log_reward.episode, label='Episode Reward')
-#plt.plot(log_reward.count_states, log_reward.mean, label='Mean of 30 episodes')
-#plt.xlabel('State-Count for Game Environment')
-#plt.legend()
-#plt.show()
-#
-#plt.plot(log_q_values.count_states, log_q_values.mean, label='Q-Value Mean')
-#plt.xlabel('State-Count for Game Environment')
-#plt.legend()
-#plt.show()
+
 agent.epsilon_greedy.epsilon_testing
 agent.training = False
 agent.reset_episode_rewards()
